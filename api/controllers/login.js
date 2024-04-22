c51887ec749e0254363a344e59320a17c10a7c49
CHICH <charbelh162@gmail.com>
2024-04-22 14:43:51 -0400

    login api functunality v1.12 login api was completed but it needs to be changed there need to add a empty password to the user exists part

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -1,8 +1,9 @@
 const {Response,UserID,UserData} = require('../models/DataModels/UserDataModel');
 const { EncryptedData } = require('../models/EncryptData/de-encrypt');
-const {readFile} = require('fs');
+const {readFile,writeFile} = require('fs');
 const utils = require('util');
 const readTables = utils.promisify(readFile);
+const writeDataBase = utils.promisify(writeFile);
 const {Error,SignInError} = require('../errors/Error');
 
 const LogIn = async (req,res)=>{
@@ -12,25 +13,45 @@ const LogIn = async (req,res)=>{
     const encryptedUser = EncryptedData(UsernameOrEmail,'EMAIL_HASHING');
     let isUserTrue = await DataUsersTable.find(user => (user.email === encryptedUser) || (user.username === UsernameOrEmail));
     let passwordEnterdEncrypted = EncryptedData(password,'PASSWORD_HASING');
-    const SignInErrors = new SignInError(!isUserTrue,UsernameOrEmail.length === 0, password.length === 0,isUserTrue.password !== passwordEnterdEncrypted);
+    const SignInErrors = new SignInError(!isUserTrue,UsernameOrEmail.length === 0, password.length === 0,( isUserTrue ? isUserTrue.password !== passwordEnterdEncrypted : true));
 
-    if(SignInErrors.ErrorExists()){
-        if(SignInErrors.EmailOrUsernameIsEmpty){
-            SignInErrors.addUsernameOrEmailMessage("")
-        }
-        if(SignInErrors.passwordIsEmpty){
-            SignInErrors.addPasswordMessage("")
-        }
-        if(SignInErrors.emailOrUsernameNotFound){
+    try {
+        if(SignInErrors.EmailOrUsernameIsEmpty || SignInErrors.emailOrUsernameNotFound || SignInErrors.passwordIsEmpty){
+            //email or username error
+            if(SignInErrors.emailOrUsernameNotFound){
+                SignInErrors.addUsernameOrEmailMessage("this email or username does not exist enter a valid email or username or create an account");
+            }
+            if(SignInErrors.EmailOrUsernameIsEmpty){
+                SignInErrors.addUsernameOrEmailMessage("Please Enter either your username or your email");
+            }
+            //
+            //password Error
+            if(SignInErrors.passwordIsEmpty){
+                SignInErrors.addPasswordMessage("Please enter your password");
+            }
+            //
+            res.status(400).json(new Error({emailOrUsernameError:SignInErrors.UsernameOrEmailErrorMessage, passwordErrror:SignInErrors.PasswordErrorMessage}))
+        }else if(isUserTrue){
 
+            if(SignInErrors.wrongPassword){
+                res.status(400).json(new Error({PasswordError:"wrong password please write a valid password"}));
+            }
+
+            else if(isUserTrue.password === passwordEnterdEncrypted){
+
+                // saving the login to the logs 
+                let logsMessage = `${req.time} User logged Into his account => UserID : ${isUserTrue.UserID}`;
+                let oldDb = await readTables('./db/logs/UsersLogs.log','utf-8');
+                logsMessage = await `${logsMessage} \n${oldDb}`;
+                await writeDataBase('./db/logs/UsersLogs.log', logsMessage, 'utf-8');
+                //
+                res.status(200).json(new Response(true,new UserID(isUserTrue.UserID, isUserTrue.userSecret,'User was succesfully logged in')));
+
+            }
         }
-        res.status(400).json(new Error({emailOrUsernameError:SignInErrors.UsernameOrEmailErrorMessage, passwordErrror:SignInErrors.PasswordErrorMessage}))
     }
-
-    if(isUserTrue){
-        if(SignInErrors.wrongPassword){
-            res.status(200).json('error')
-        }
+    catch(err){
+        console.log(err)
     }
     
 }
